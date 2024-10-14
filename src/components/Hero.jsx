0b7350df93df9a7a97b5ab653f46146b200ee517
Transa1 <kevinx.martinez.haro@gmail.com>
2024-10-14 14:59:24 -0700

    Added Download Résumé Button

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,6 +1,8 @@
 import { HERO_CONTENT } from '../constants';
 import profilePic from '../assets/Kevin.jpg';
 import { motion } from 'framer-motion';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faCloudDownload } from '@fortawesome/free-solid-svg-icons';
 
 const container = (delay) => ({
     hidden: { x: -100, opacity: 0 },
@@ -27,14 +29,18 @@ const Hero = () => {
                         Data Scientist & Machine Learning Researcher
                     </motion.span>
                     <motion.p variants={container(1)} initial='hidden' animate='visible' className='my-2 max-w-xl py-6 font-light tracking-tighter'>{HERO_CONTENT}</motion.p>
+                    <motion.a variants={container(1.5)} initial='hidden' animate='visible' href="src/assets/CV Kevin Martínez.pdf" download  className='no-underline tracking-thin text-white font-medium'>
+                        <FontAwesomeIcon icon={faCloudDownload} className='mr-2' />
+                        Download my résumé
+                    </motion.a>
                 </div>
                 <div className='w-full lg:w-1/2 lg:p-8'>
                     <div className='flex justify-center'>
-                        <motion.img initial={{x: 100, opacity: 0}}
-                        animate={{x: 0, opacity: 1}}
-                        transition={{delay: 1, duration: 0.7}}
-                        className='rounded-2xl'
-                        src={profilePic} alt="Kevin Martínez" />
+                        <motion.img initial={{ x: 100, opacity: 0 }}
+                            animate={{ x: 0, opacity: 1 }}
+                            transition={{ delay: 1, duration: 0.7 }}
+                            className='rounded-2xl'
+                            src={profilePic} alt="Kevin Martínez" />
                     </div>
                 </div>
             </div>
