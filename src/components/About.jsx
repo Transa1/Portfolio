dc4e2abe5c4406d01514fa65c019b13eb8a8f69b
Transa1 <kevinx.martinez.haro@gmail.com>
2024-12-01 03:19:53 -0700

    Removed Video

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -23,12 +23,8 @@ const About = () => {
             </h1>
             <div className="flex flex-wrap">
                 <motion.div whileInView={{ opacity: 1, x: 0 }} initial={{ opacity: 0, x: -100 }} transition={{ duration: 0.5 }} className="w-full lg:w-1/2 lg:p-8">
-                    <div className="relative flex items-center justify-center cursor-pointer" onClick={openModal}>
+                    <div className="relative flex items-center justify-center">
                         <img className="rounded-2xl" src={aboutImg} alt="about" />
-                        {/* Contenedor para el ícono */}
-                        <div className="absolute inset-0 flex items-center justify-center bg-black bg-opacity-50 rounded-2xl">
-                            <FaPlay className="text-white text-4xl" /> {/* Ícono de play */}
-                        </div>
                     </div>
                 </motion.div>
                 <motion.div whileInView={{ opacity: 1, x: 0 }} initial={{ opacity: 0, x: 100 }} transition={{ duration: 0.5 }} className="w-full lg:w-1/2">
@@ -47,35 +43,7 @@ const About = () => {
                 </motion.div>
             </div>
 
-            {/* Modal */}
-            {isModalOpen && (
-                <div 
-                    className="fixed inset-0 bg-black bg-opacity-50 flex items-center justify-center z-50"
-                    onClick={handleOutsideClick}
-                >
-                    <div 
-                        className="bg-white p-8 rounded-lg relative max-w-3xl w-full"
-                        onClick={(e) => e.stopPropagation()} 
-                    >
-                        <button 
-                            className="absolute top-2 right-2 text-black" 
-                            onClick={closeModal}
-                        >
-                            ✖
-                        </button>
-                        <div className="relative pb-[56.25%] h-0"> {/* 16:9 Aspect Ratio */}
-                            <iframe
-                                className="absolute top-0 left-0 w-full h-full"
-                                src="/VideoCV.mp4"
-                                title="Personal Video"
-                                frameBorder="0"
-                                allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
-                                allowFullScreen
-                            />
-                        </div>
-                    </div>
-                </div>
-            )}
+           
         </div>
     );
 }
